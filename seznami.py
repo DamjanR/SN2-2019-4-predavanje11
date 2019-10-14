590c269d3e5079146db71da1d8c556e38a47ee73
DR <damjan.ramsak@gmail.com>
2019-10-14 18:07:37 +0200

    Dopolnil seznam

--- seznami.py
+++ seznami.py
@@ -3,5 +3,25 @@ stevila = [5, 2, 8, 3]
 # izpis vseh števil
 print(stevila)
 
+#Urejanje seznama
+stevila.sort()
+print("Števila smo uredili")
+
+# izpis urejenih števl
+print(stevila)
+
 # izpis števila na mestu 1
 print(stevila[1])
+
+# izpis števila vseh elementov
+print("Število vseh elementov je " + str(len(stevila)))
+
+# doda število ampak vedno na konc seznama
+stevila.append(7)
+# zbriše vrednost tri ne na mestu tri!
+stevila.remove(3)
+# zbriše PRAV element na MESTU 0 (nič) TO JE 2!!!
+del stevila[0]
+
+print("spremenjen seznam:")
+print(stevila)
